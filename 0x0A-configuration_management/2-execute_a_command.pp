f2fdf003d8e3babe8c6a04f4a84d0ae90ace9869
Marksman Green <x17.ebi@icloud.com>
2024-06-08 13:35:08 +0100

    Install a package

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,10 @@
 # a puppet manifest to kill a process
 
-exec { 'kill killmenow':
-  command => '/usr/bin/pkill killmenow',
-  path    => '/bin/pkill',
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0',
+}
+
+exec { 'erify-flask-version':
+  command => '/usr/local/bin/flask --version',
+  require => Exec['install-flask'],
 }
