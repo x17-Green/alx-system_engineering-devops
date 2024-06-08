6f280db91b8f4637e01dbb7ecac1d949f99ac1ae
Marksman Green <x17.ebi@icloud.com>
2024-06-08 12:34:01 +0100

    Create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file using puppet
+file { '/tmp/school':
+  ensure  => present,
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
