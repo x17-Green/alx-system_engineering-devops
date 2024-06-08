8df0ce2c3b1dbe380e5caffd06e1d0bb0f35516a
Marksman Green <x17.ebi@icloud.com>
2024-06-08 13:49:48 +0100

    Updated code to execute a kill command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,6 @@
 # a puppet manifest to kill a process
 
-exec { 'install-flask':
-  command => 'pip3 install flask==2.1.0',
-}
-
-exec { 'erify-flask-version':
-  command => '/usr/local/bin/flask --version',
-  require => Exec['install-flask'],
+exec { 'kill killmenow':
+  command => '/usr/bin/pkill killmenow',
+  path    => '/bin/pkill',
 }
