8b1955185e470f14ec8177358bf617e1a39e531e
Marksman Green <x17.ebi@icloud.com>
2024-06-08 12:01:09 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # puppet file to configure the ssh config file
 file {'/etc/ssh/ssh_config':
-  ensure => present,
+  ensure  => present,
   content => "
 Host school
     HostName 100.25.140.95
@@ -10,7 +10,7 @@ Host school
     PubkeyAuthentication yes
     IdentitiesOnly yes
 ",
-  owner => 'green',
-  group => 'green',
-  mode => '0600',
+  owner   => 'green',
+  group   => 'green',
+  mode    => '0600',
 }
